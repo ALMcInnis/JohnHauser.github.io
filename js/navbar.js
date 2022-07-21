529ab6faf516214cc674e25996823dd9140e44d5
Anthony L. McInnis <amcinnis82@gmail.com>
2022-07-21 15:17:02 -0400

    Add files via upload
    Uploaded seven files.

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+﻿
+function toggleNavbar() {
+  var nav = document.getElementById("vertical-nav");
+  if (nav.className === "navbar") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "navbar";
+  }
+}
---
+++ js/scrollspy-vertical.js
@@ -0,0 +1,22 @@
+﻿let menuSection=document.querySelectorAll('menu li');
+menuSection.forEach((v)=>{
+	v.onclick =(()=>{
+		setTimeout(()=>{
+			menuSection.forEach(j=>j.classList.remove('active'))
+			v.classList.add('active')
+		},50)
+	})
+})
+
+window.onscroll=(()=> {
+	let section = document.querySelectorAll('.content');
+	section.forEach((v,i)=> {
+		let rect = v.getBoundingClientRect().y;
+		if(rect < window.innerHeight-400){
+			menuSection.forEach(v=>
+				v.classList.remove('active'))
+				menuSection[i].classList.add('active')
+			}
+		})
+	})
+
